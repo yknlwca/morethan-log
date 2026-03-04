5612e645f80946b761478cbf228bf9cd45914821
KWANGLIM <choikl3115@naver.com>
2026-03-04 16:30:37 +0900

    260304-1630

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,9 +8,9 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link
-            rel="apple-touch-icon"
+            rel="notion-face"
             sizes="192x192"
-            href="/apple-touch-icon.png"
+            href="/notion-face.png"
           ></link>
           <link
             rel="alternate"
